188aedd9b219ea164c71242ea048534047b79eef
Sadaf Kausar <137484958+SadafKausar2025@users.noreply.github.com>
2024-08-30 12:11:57 +0530

    Add files via upload

---
+++ app/[username]/page.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PaymentPage from "@/components/PaymentPage";
+import { notFound } from "next/navigation";
+import User from "../models/User";
+import connectDb from "../db/connectDb";
+
+const Username = async ({ params }) => {
+  const checkUser = async () => {
+    await connectDb();
+    let u = await User.findOne({ username: params.username });
+    if (!u) {
+      return notFound();
+    }
+  };
+
+  await checkUser();
+
+  return (
+    <>
+      <PaymentPage username={params.username} />
+    </>
+  );
+};
+
+export default Username;
+
+export async function generateMetadata({ params }) {
+  return {
+    title: `Support ${params.username} - Get Me A Chai`,
+  };
+}
---
+++ app/dashboard/page.js
@@ -0,0 +1,7 @@
+import Dashboard from "@/components/Dashboard";
+
+const dashboardPage = () => {
+  return <Dashboard />;
+};
+
+export default dashboardPage;
